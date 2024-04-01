a09b173e74fd0028c683d414e34ba5feddea22e3
eduzinnsp <storee.fantasy@gmail.com>
2024-04-01 18:18:58 -0300

    First Commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Discord Farm - Tool Boxes 
+
+🚩 Version: 1.0 (BETA)
+
+
+🚀 Automatic ToolBoxes generator, if you already have it, it ignores it and starts the system again.
+
+❌ No have proxy in version beta.
+
+#
+✨ Discord Support: [Click Here](https://discord.gg/awsxu)
+
+👑 Developer: @ikrcl (das) or [Send Message](https://discord.com/channels/@me/1179154326170636400)
---
+++ src/farm.ts
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+class DiscordLootBoxes {
+    config: any;
+
+    constructor () {
+        this.config = {
+            token: "YOUR ACCOUNT TOKEN"
+        }
+
+        this.getLootBoxes();
+    }
+
+    async getLootBoxes() {
+        const req = await axios.request({
+            url: "https://discord.com/api/v9/users/@me/lootboxes",
+            method: "GET",
+            headers: {
+                "Authorization": this.config.token,
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+                "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEyMy4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTIzLjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjI4MDQ3MiwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0="
+            }
+        })
+
+        if (req.status as number === 200) {
+           const id = await this.openLootBoxes();
+           const stringfied = JSON.stringify(req.data.opened_items).includes(id);
+
+           if (stringfied) {
+            console.log(`Você já possui este item - [${id}]`) 
+            return new DiscordLootBoxes();
+           } else {
+            console.log(`Novo item desbloqueado - [${id}]`);
+            return new DiscordLootBoxes();
+           }
+        }
+    }
+
+    async openLootBoxes() {
+        const req = await axios.request({
+            url: "https://discord.com/api/v9/users/@me/lootboxes/open",
+            method: "POST",
+            headers: {
+                "Authorization": this.config.token,
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+                "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEyMy4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTIzLjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjI4MDQ3MiwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0="
+            }
+        })
+
+        return req.data.opened_item;
+    }
+}
+
+new DiscordLootBoxes();
